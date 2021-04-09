7b2ef68f7de1d696babd4a89688aa05861679963
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-04-09 20:04:58 -0300

    Add files via upload
    adicionado as primeiras classes

---
+++ Aluno.java
@@ -0,0 +1,23 @@
+package br.com.caelum.ed;
+
+public class Aluno {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public boolean equals(Object objeto){
+		Aluno outroAluno = (Aluno)objeto;
+		return this.nome.equals(outroAluno.nome);
+	}
+	
+	public String toString(){
+		return "Nome: "+ this.nome;
+	}
+}
